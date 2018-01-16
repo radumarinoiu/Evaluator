6a4425e06860085dba281233073dfbfafc53fbb9
Unknown <dragos_d111@yahoo.com>
2018-01-16 12:23:59 +0200

    Funcions

--- EVAL/SelectFunction.cpp
+++ EVAL/SelectFunction.cpp
@@ -1,4 +1,4 @@
-//*Abs, acos, asin, atan, ceiling, cos, floor, log, log10, max, min, pow, round, sign, sqrt, tan, truncate
+//Abs, acos, asin, atan, ceiling, cos, floor, log, log10, max, min, pow, round, sign, sqrt, tan, truncate
 #include<iostream>
 #include<vector>
 #include<cmath>
@@ -15,12 +15,42 @@ Variable SelectFunction(int n, vector<Variable> Variables)
 		v.value = abs(v.value);
 		return v;
 	}
-	if (n == 2) return acos(Elements);
-	if (n == 3) return asin(Elements);
-	if (n == 4) return atan(Elements);
-	if (n == 5) return ceil(Elements);
-	if (n == 6) return cos(Elements);
-	if (n == 7) return floor(Elements);
+	if (n == 2)
+	{
+		Variable v = Variables[0];
+		v.value = acos(v.value);
+			return v;
+	}
+	if (n == 3)
+	{
+		Variable v = Variables[0];
+		v.value = asin(v.value);
+			return v;
+	}
+	if (n == 4) 
+	{
+		Variable v = Variables[0];
+		v.value = atan(v.value);
+			return v;
+	}
+	if (n == 5) 
+	{
+		Variable v = Variables[0];
+		v.value = ceil(v.value);
+			return v;
+	}
+	if (n == 6) 
+	{
+		Variable v = Variables[0];
+		v.value = cos(v.value);
+			return v;
+	}
+	if (n == 7)
+	{
+		Variable v = Variables[0];
+		v.value = floor(v.value);
+			return v;
+	}
 	if (n == 8)
 	{
 		Variable v = Variables[0];
@@ -35,14 +65,61 @@ Variable SelectFunction(int n, vector<Variable> Variables)
 		return v;
 
 	}
-	if (n == 9) return log10(Elements) // Included in log
-	if (n == 10) return fmax(Elements);
-	if (n == 11) return fmin(Elements);
-	if (n == 12) return pow(Elements);
-	if (n == 13) return round(Elements);
-	if (n == 14) return sign(Elements);
-	if (n == 15) return sin(Elements);
-	if (n == 16) return sqrt(Elements);
-	if (n == 17) return tan(Elements);
-	if (n == 18) return trunc(Elements);
+	if (n == 9) 
+	{
+		Variable v1 = Variables[0];
+		Variable v2 = Variables[1];
+		v1.value = fmax(v1.value, v2.value);
+			return v1;
+	}
+	if (n == 10) 
+	{
+		Variable v1= Variables[0];
+		Variable v2 = Variables[1];
+			v1.value = fmin(v1.value, v2.value);
+			return v1;
+	}
+	if (n == 11) 
+	{
+		Variable v = Variables[0];
+		Variable power = Variables[1];
+		v.value = pow(v.value, power.value);
+			return v;
+	}
+	if (n == 12) 
+	{
+		Variable v = Variables[0];
+		v.value = round(v.value);
+			return v;
+	}
+	if (n == 13) 
+	{
+		Variable v = Variables[0];
+		v.value = v.value / abs(v.value);
+			return v;
+	}
+	if (n == 14) 
+	{
+		Variable v = Variables[0];
+		v.value = sin(v.value);
+			return v;
+	}
+	if (n == 15) 
+	{
+		Variable v = Variables[0];
+		v.value = sqrt(v.value);
+			return v;
+	}
+	if (n == 16) 
+	{
+		Variable v = Variables[0];
+		v.value = tan(v.value);
+			return v;
+	}
+	if (n == 17) 
+	{
+		Variable v = Variables[0];
+		v.value = trunc(v.value);
+			return v;
+	}
 }
